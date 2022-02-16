8a765ec96c7ff6093b74fe5de15464e1b0c2884f
sergioPereiraBR <sergiopereira.br@hotmail.com>
2022-02-16 17:40:06 -0300

    Totem gateway project started

---
+++ src/main/java/com/totemstorage/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.totemstorage.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
