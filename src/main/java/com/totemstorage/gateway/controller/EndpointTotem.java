a19bc617ebcb55f5f0deaeaf0d18b8fa368b695f
sergioPereiraBR <sergiopereira.br@hotmail.com>
2022-02-22 10:25:03 -0300

    update

--- src/main/java/com/totemstorage/gateway/controller/EndpointTotem.java
+++ src/main/java/com/totemstorage/gateway/controller/EndpointTotem.java
@@ -11,7 +11,7 @@ import com.totemstorage.gateway.services.GatewayService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
-@CrossOrigin
 @RestController
 @RequestMapping("/totem")
 public class EndpointTotem {
