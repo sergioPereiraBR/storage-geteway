bba3b3de59350272d96e1dff9a8da4a0f5a6a8b3
sergioPereiraBR <sergiopereira.br@hotmail.com>
2022-02-22 17:01:22 -0300

    to deploy

--- src/main/java/com/totemstorage/gateway/services/GatewayService.java
+++ src/main/java/com/totemstorage/gateway/services/GatewayService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GatewayService {
     private static final String GATEWAY_FAILURE = "Totem Storage Gateway: connection failed.";
+    private boolean blobExists = false;
 
     public String blobStorage(String totemPacketFileName, String totemPacketData) {
 
@@ -27,16 +28,18 @@ public class GatewayService {
 
             BlobServiceClient storageClient = new BlobServiceClientBuilder().endpoint(endpoint).credential(credential).buildClient();
             BlobContainerClient blobContainerClient = storageClient.getBlobContainerClient("sandbox/safety_analytics/totem/relatos");
-            if(!blobContainerClient.getBlobClient(totemPacketFileName).exists()){
-                BlockBlobClient blobClient = blobContainerClient.getBlobClient(totemPacketFileName).getBlockBlobClient();
+            blobExists = blobContainerClient.getBlobClient(totemPacketFileName).exists()
+            BlockBlobClient blobClient = blobContainerClient.getBlobClient(totemPacketFileName).getBlockBlobClient();
 
-                InputStream dataStream = new ByteArrayInputStream(totemPacketData.getBytes(StandardCharsets.UTF_8)); 
-                blobClient.upload(dataStream, totemPacketData.length());
-                dataStream.close();
-            } else throw new ExceptionGateway("Erro ao tentar adicionar, o blob já existe.");
+            InputStream dataStream = new ByteArrayInputStream(totemPacketData.getBytes(StandardCharsets.UTF_8)); 
+            blobClient.upload(dataStream, totemPacketData.length());
+            dataStream.close();
+         
         
         } catch (Exception e) {
-            throw new ExceptionGateway(GATEWAY_FAILURE + e.getMessage());
+            if(!blobExists)
+                throw new ExceptionGateway(GATEWAY_FAILURE + e.getMessage())
+            else throw new ExceptionGateway("Erro ao tentar adicionar, o blob já existe.");
         }
 
         return totemPacketFileName;
