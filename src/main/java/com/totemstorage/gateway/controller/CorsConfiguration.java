453d77487addb84e87a6ab665e9a95258ad9c096
sergioPereiraBR <sergiopereira.br@hotmail.com>
2022-02-22 09:59:49 -0300

    endpoint set to http://localhost:4200

--- src/main/java/com/totemstorage/gateway/controller/CorsConfiguration.java
+++ src/main/java/com/totemstorage/gateway/controller/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://localhost:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
